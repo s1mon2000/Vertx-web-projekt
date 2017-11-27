cb5ac35f234f89f7f6aa76b7b7cb7db4c6517154
s.hoenes <s.hoenes@IR1-PC-13.musterschule.schule.paedml>
2017-11-27 17:18:40 +0100

    session wird erstellt

--- src/main/java/de/qreator/vertx/VertxWebFormular.java
+++ src/main/java/de/qreator/vertx/VertxWebFormular.java
@@ -12,7 +12,12 @@ import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.Session;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.ext.web.sstore.SessionStore;
 
 public class VertxWebFormular {
 
@@ -22,27 +27,36 @@ public class VertxWebFormular {
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
+        SessionStore store = LocalSessionStore.create(vertx);
+
+        SessionHandler sessionHandler = SessionHandler.create(store);
+
+        router.route().handler(CookieHandler.create());
+        router.route().handler(sessionHandler);
 
         router.route("/anfrage").handler(routingContext -> {
+
             String typ = routingContext.request().getParam("typ");
             String name = routingContext.request().getParam("name");
-             String passwort = routingContext.request().getParam("passwort");
+            String passwort = routingContext.request().getParam("passwort");
             HttpServerResponse response = routingContext.response();
             response.putHeader("content-type", "application/json");
             JsonObject jo = new JsonObject();
+            
+            Session session = routingContext.session();
+            session.put("alter", "27");
+            String alter=session.get("alter");
 
-            if (typ.equals("namenKnopf") && passwort.equals  ("safetyfirst")) {
+            if (typ.equals("namenKnopf") && passwort.equals("safetyfirst")) {
                 jo.put("typ", "antwort");
-                jo.put("text", "Der Benutzer ist " + name);        
+                jo.put("text", "Der Benutzer ist " + name);
                 jo.put("passwort", "Das Passwort war " + passwort);
-            }
-            else{
+            } else {
                 jo.put("text", "Volltrottel: " + name);
                 jo.put("passwort", " Falsches Passwort");
             }
             response.end(Json.encodePrettily(jo));
         });
-    
 
         // statische html-Dateien werden über den Dateipfad static ausgeliefert
         router.route("/static/*").handler(StaticHandler.create().setDefaultContentEncoding("UTF-8"));
